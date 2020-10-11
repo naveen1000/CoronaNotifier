6771a773adf0a310fafc2c8a280f75b00a6c2f39
Simma Naveen Kumar <43130444+naveen1000@users.noreply.github.com>
2020-10-11 19:44:25 +0530

    Fully Updated corona notifier data sent

--- coronaNotifier.py
+++ coronaNotifier.py
@@ -1,47 +1,56 @@
 import requests
 import json
-
 def notify(msg):
-    #Telegam Bot Url
-    #url='https://api.telegram.org/bot879982304:AAHG7ZRyEMWoQB-ToaiJBv_gMvkW-ekJcSg/sendMessage?chat_id=582942300&text='+msg
-    #TelegramChannel chatId -1001181667975
-    url='https://api.telegram.org/bot879982304:AAHG7ZRyEMWoQB-ToaiJBv_gMvkW-ekJcSg/sendMessage?chat_id=-1001181667975&text='+msg
+    #TelegramChannel chatId -1001181667975   
+    #MyChatId 582942300
+    url='https://api.telegram.org/bot1193312817:AAGTRlOs3YZHFeDSO_33YTwwewrEaMbLizE/sendMessage?chat_id=582942300&parse_mode=Markdown&text='+msg
     requests.get(url)
     print("notified")
 
 raw = requests.get('https://api.covid19india.org/data.json')
 data = raw.json()
 daily_report = data['cases_time_series'][-1]
-msg='*****Corona Update*****\n\n'
-for i in daily_report:
-	msg = msg + i + ' : ' + daily_report[i] + '\n'
-
-lastupdatedtime = data['key_values'][0]['lastupdatedtime']
-msg = msg +'updatetime : ' + lastupdatedtime 
+msg='--------------*****Corona Update*****--------------\n'
 
 
-msg=msg +'\n\n'
-msg=msg +'--------------Statewise--------------\n' 
-msg=msg +'confirm - death - recover - active - updatetime - state \n\n'
 statewise = data['statewise']
 for i in statewise:
-	active = i['active']
-	confirmed = i['confirmed']
-	deaths = i['deaths']
-	recovered = i['recovered']
-	state = i['state']
-	lastupdatedtime = i['lastupdatedtime']
-	msg = msg + confirmed + ' ' + deaths + ' ' + recovered + ' ' + active + '  ' + lastupdatedtime + ' ' + state[0:14] + '\n'
-
-msg=msg +'\n'
-msg=msg +'--------------AP Districtwise--------------\n\n' 
+    state = i['state']
+    if state == "Andhra Pradesh":
+        active = i['active']
+        confirmed = i['confirmed']
+        deaths = i['deaths']
+        recovered = i['recovered']
+        state = i['state']
+        lastupdatedtime = i['lastupdatedtime']
+        deltaconfirmed = i['deltaconfirmed']
+        deltadeaths = i['deltadeaths']
+        deltarecovered = i['deltarecovered']
+        msg= msg + "***Andhra Pradesh***"+"\n"
+        msg = msg + "Today Confirmed:" + deltaconfirmed + "\n"
+        msg = msg + "Today Dead:" + deltadeaths + "\n"
+        msg = msg + "Today Recovered:" + deltarecovered + "\n"
+        msg= msg + "-------------------------\n"
+        msg= msg + "Active:" + active +"\n"
+        msg= msg + "Confirmed:" + confirmed +"\n"
+        msg= msg + "Deaths:" + deaths +"\n"
+        msg= msg + "Recovered:" + recovered +"\n"
+        msg= msg + "UpdatedOn:" + lastupdatedtime +"\n\n"
+        break
+	
+msg = msg +'--------------***AP District Confirmed ***--------------\n' 
+msg = msg +'Today  Total    State \n' 
 raw = requests.get('https://api.covid19india.org/state_district_wise.json')
 data = raw.json()
 
 districtData = data['Andhra Pradesh']['districtData']
 for district in districtData:
-	msg =msg + str(districtData[district]['confirmed']) + ' ' + district +'\n'
+	msg =msg + str(districtData[district]['delta']['confirmed']).center(3) + str(districtData[district]['confirmed']).center(6) + ' ' + district +'\n'
 
+msg=msg +'\n\n'
+msg=msg +'--------------***Yestrday Report***--------------\n\n' 	
+
+for i in daily_report:
+		msg = msg + i.capitalize().replace('deceased',"dead").replace('Daily',"Yesterday") + ' : ' + daily_report[i] + '\n'	
 print(msg)
 notify(msg)	
-		
